ae9fe111f9e14ad08b3595db544ea2215571f6c7
Irostovsky <ivan.rostovsky@gmail.com>
2025-06-13 09:24:51 +0300

    Load all events.

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,5 +1,43 @@
+import { useEffect, useState } from "react";
+import EventsList from "../components/EventsList";
+
 function EventsPage() {
-  return <h1>Events Page</h1>;
+  const [events, setEvents] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchEvents() {
+      try {
+        const response = await fetch("http://localhost:8080/events");
+        if (!response.ok) {
+          throw new Error("Failed to fetch events");
+        }
+        const data = await response.json();
+        setEvents(data.events);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchEvents();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  return (
+    <>
+      <EventsList events={events} />
+    </>
+  );
 }
 
 export default EventsPage;
