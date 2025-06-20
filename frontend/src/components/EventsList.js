c48eea816b93357d516081afa142e07707cf0e3a
Irostovsky <ivan.rostovsky@gmail.com>
2025-06-20 11:04:08 +0300

    Use loader to load the page data.

--- frontend/src/components/EventsList.js
+++ frontend/src/components/EventsList.js
@@ -1,7 +1,8 @@
-import { Link } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import classes from "./EventsList.module.css";
 
-function EventsList({ events }) {
+function EventsList() {
+  const events = useLoaderData();
   return (
     <div className={classes.events}>
       <h1>All Events</h1>
